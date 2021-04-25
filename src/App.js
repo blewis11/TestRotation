c0371e37f91791f13479186a039a2d580207fcb8
blewis11 <bejallewis@hotmail.com>
2021-04-25 13:42:00 +0200

    add two models and disable zoom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useState, useEffect } from "react";
 import "./App.scss";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
 import { useFBXLoader, Html } from "drei";
 import CameraControls from "camera-controls";
-import Header from "./components/header";
 import * as THREE from "three";
 
 const clock = new THREE.Clock();
@@ -49,32 +48,73 @@ const Model = ({ position, modelRef, rotationY }) => {
   );
 };
 
-const WithCameraControlers = ({ position, modelRef }) => {
-  const { camera, gl } = useThree();
+const Model2 = ({ position, modelRef, rotationY }) => {
+  const object = useFBXLoader("model.1.fbx");
+
+  return (
+    <>
+      <primitive
+        object={object}
+        ref={modelRef}
+        position={position}
+        rotation={[0, rotationY, 0]}
+      />
+    </>
+  );
+};
+
+const EPS = 1e-5;
+
+const WithCameraControlers = ({ position, modelRef, modelRef1 }) => {
+  const { camera, scene, gl } = useThree();
   const cameraControls = new CameraControls(camera, gl.domElement);
+  cameraControls.dollySpeed = 0;
+  cameraControls.azimuthRotateSpeed = 0.3; // negative value to invert rotation direction
+  cameraControls.polarRotateSpeed = -0.3; // negative value to invert rotation direction
+  cameraControls.truckSpeed = (1 / EPS) * 3;
+  cameraControls.mouseButtons.wheel = CameraControls.ACTION.ZOOM;
+  cameraControls.touches.two = CameraControls.ACTION.TOUCH_ZOOM_TRUCK;
+  cameraControls.saveState();
 
   useFrame(() => {
     const delta = clock.getDelta();
     cameraControls.update(delta);
   });
 
-  return (
-    <>
-      <Html>
-        <button
-          style={{ width: "100px" }}
-          onClick={() => {
-            var current = modelRef.current;
-            console.log({ current });
-            cameraControls.fitToBox(modelRef.current, true);
-            cameraControls.rotateTo(0, 1.56, true);
-          }}
-        >
-          Click Me
-        </button>
-      </Html>
-    </>
-  );
+  useEffect(() => {
+    const onClick = event => {
+      var mouse = new THREE.Vector2();
+
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      const raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera(mouse, camera);
+
+      let intersects = raycaster.intersectObjects(scene.children, true);
+
+      if (intersects.length > 0) {
+        let matchingIntersects1 = intersects.filter(
+          item => item.object.parent.uuid === modelRef.current.uuid
+        );
+        let matchingIntersects2 = intersects.filter(
+          item => item.object.parent.uuid === modelRef1.current.uuid
+        );
+
+        if (matchingIntersects1.length > 0) {
+          cameraControls.fitToBox(modelRef.current, true);
+          cameraControls.rotateTo(0, 1.56, true);
+        } else if (matchingIntersects2.length > 0) {
+          cameraControls.fitToBox(modelRef1.current, true);
+          cameraControls.rotateTo(0, 1.56, true);
+        }
+      }
+    };
+
+    document.addEventListener("click", onClick, false);
+  });
+
+  return null;
 };
 
 function randomNumber(min, max) {
@@ -83,23 +123,25 @@ function randomNumber(min, max) {
 }
 
 export default function App() {
-  const positionX = randomNumber(-100, 100);
-  const positionY = randomNumber(-50, 50);
-  const positionZ = randomNumber(-50, 50);
+  const positionX = randomNumber(-200, 200);
+  const positionY = randomNumber(-200, 200);
+  const positionZ = randomNumber(-200, -100);
+
+  const positionX1 = randomNumber(-200, 200);
+  const positionY1 = randomNumber(-50, 50);
+  const positionZ1 = randomNumber(-50, 50);
 
   const rotationY = 0;
 
-  const positionX2 = randomNumber(-100, 100);
-  const positionY2 = randomNumber(-50, 50);
-
   const modelRef = useRef();
+  const modelRef1 = useRef();
 
   return (
     <>
       <Canvas
         concurrent
         colorManagement
-        camera={{ position: [positionX2, positionY2, 120], fov: 70 }}
+        camera={{ position: [0, 0, EPS], fov: 70 }}
       >
         <Lights />
         <Suspense fallback={null}>
@@ -108,11 +150,16 @@ export default function App() {
             modelRef={modelRef}
             rotationY={rotationY}
           />
-          {/* <OrbitControls /> */}
+          <Model2
+            position={[positionX1, positionY1, positionZ1]}
+            modelRef={modelRef1}
+            rotationY={rotationY}
+          />
         </Suspense>
         <WithCameraControlers
           position={[positionX, positionY, positionZ]}
           modelRef={modelRef}
+          modelRef1={modelRef1}
         />
       </Canvas>
     </>
